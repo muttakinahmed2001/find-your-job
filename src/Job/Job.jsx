89a647c703de6c79dfda6806b927ebba1d4857c6
unknown <muttakinahmed871@gmail.com>
2023-04-12 02:59:00 +0600

    data showing of featured section

--- src/Job/Job.jsx
+++ src/Job/Job.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import './Job.css'
 
 const Job = ({job}) => {
     const {jobName,img,companyName,jobType,jobTime,location,salary}=job
     return (
-        <div className=' col-md-6'>
-            <img src={img} alt="" />
-            <h1>{jobName}</h1>
+        <div className='job'>
+            <img className='company-img' src={img} alt="" />
+            <h1 className='job-name'>{jobName}</h1>
+            <h4 className='company-name'>{companyName}</h4>
+            <section className='d-flex gap-2'>
+                <button className='border border-primary btn-job'>{jobType}</button>
+                <button className='border border-primary btn-job'>{jobTime}</button>
+            </section>
+            <section className='d-flex gap-4 mb-3'>
+                <p className='location'><img className='location-img' src="../../public/images/All Images/location.png" alt="" />{location}</p>
+                <p className='salary'>Salary:{salary}</p>
+                 
+            </section>
+            <button className='btn-details'>View Details</button>
+            
         </div>
     );
 };
--- src/components/Careers/Careers.jsx
+++ src/components/Careers/Careers.jsx
@@ -6,17 +6,20 @@ import Job from '../../Job/Job';
 const Careers = () => {
     const [jobs,setJobs]=useState([]);
     useEffect(()=>{
-        fetch('career.json')
+        fetch('/career.json')
         .then(res=>res.json())
         .then(data => setJobs(data))
     },[])
     const newJobs =jobs.slice(0,4)
     
     return (
-        <div className='careers'>
-          {newJobs.map(job => <Job
+        <div className='d-flex flex-column justify-content-center'>
+            <div className='careers'>{newJobs.map(job => <Job
           key={job.id}
-          job={job}></Job>)}
+          job={job}></Job>)}</div>
+          
+         <section > <button className='btn-see-all'>See All Jobs</button></section>
+         
         </div>
     );
 };
